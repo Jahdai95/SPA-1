029429e7295489847b00932c7a7148c72cec6046
Jahdai Reyes <jreyesortega@quaresitsolutions.com>
2019-08-23 17:35:24 -0300

    App SPA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//Rutas 
+import {APP_ROUTING } from './app.routes';
+// Servicios
+import {HeroesService} from './services/heroes.service'
 
+
+// Componentes
 import { AppComponent } from './app.component';
+import {NavbarComponent} from './componets/navbar/navbar.component';
+import {HomeComponent} from './componets/home/home.component';
+import { HeroesComponent } from './componets/heroes/heroes.component';
+import { AboutComponent } from './componets/about/about.component';
+
+// import { from } from 'rxjs';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    HeroesComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
